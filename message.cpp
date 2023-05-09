560f6bd218246ff76b2093d8b67ebcd6b6ae358f
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2023-05-09 03:12:56 +0200

    Added getters for private fields of Message class

--- message.cpp
+++ message.cpp
@@ -1,10 +1,29 @@
 #include "message.h"
 
-
-core::Message::Message(const QString &title, const QString &sender, const QString &receiver,
+namespace core {
+Message::Message(const QString &title, const QString &sender, const QString &receiver,
                        const QString &content, QObject *parent)
     : mSender{sender}, mReceiver{receiver}, mContent{content}, QObject(parent)
 {
-    if (title.isNull() or title.isEmpty())
+    if (title.isNull() || title.isEmpty())
         mTitle = QString();
 }
+
+QString Message::getTitle() const {
+    return mTitle;
+}
+
+QString Message::getSender() const {
+    return mSender;
+}
+
+QString Message::getReceiver() const {
+    return mReceiver;
+}
+
+QString Message::getContent() const {
+    return mContent;
+}
+
+}
+
--- message.h
+++ message.h
@@ -11,6 +11,10 @@ public:
     explicit Message(const QString& title, const QString& sender, const QString& receiver,
                      const QString& content, QObject *parent = nullptr);
 
+    QString getTitle() const;
+    QString getSender() const;
+    QString getReceiver() const;
+    QString getContent() const;
 signals:
 
 private:
