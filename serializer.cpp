afcb41763c10c54a11d66e71190fd7d7e5f04c16
Mateusz Szybicki <mateusz.szybicki@interia.pl>
2023-05-09 10:11:43 +0200

    Refactor Serializer class

--- serializer.cpp
+++ serializer.cpp
@@ -1,5 +1,4 @@
 #include "serializer.h"
-#include <QFile>
 
 Serializer::Serializer(QObject *parent)
     : QObject{parent}
@@ -11,15 +10,12 @@ QByteArray Serializer::Serialize(core::Message *message) const
 {
     QByteArray result;
     QVariantMap map;
-    QFile file("test.txt");
-    file.open(QIODevice::WriteOnly);
     map["title"] = message->getTitle();
     map["sender"] = message->getSender();
     map["receiver"] = message->getReceiver();
     map["content"] = message->getContent();
     QJsonDocument document = QJsonDocument::fromVariant(map);
     result = document.toJson();
-    file.write(document.toJson());
 
     return result;
 }
